0909dcd9f6dfb86baddc140750ef3a0f6b621af9
fab-ryan <royalfabrice123@gmail.com>
2021-01-23 20:58:26 +0200

    your file of java

---
+++ packageExample1.java
@@ -0,0 +1,13 @@
+package package13;
+public class packageExample1{
+    int height;
+    int width;
+    public packageExample1()
+    {
+        height=4;
+        width=6;
+        int area =height*width;
+        System.out.println("the area od rectangle is" + area);
+    }
+
+}
